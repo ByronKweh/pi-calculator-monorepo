e363ad91ad898173d42f34d15e35d42ef111534b
ByronKweh <byronkweh@gmail.com>
2023-02-18 21:20:32 +0800

    working backend with prettier

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom';
-import { ErrorBoundary } from 'react-error-boundary';
+import { StrictMode } from "react";
+import * as ReactDOM from "react-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
-import App from './app/app';
-import { ErrorFallback } from './util/ErrorFallback';
+import App from "./app/app";
+import { ErrorFallback } from "./util/ErrorFallback";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <StrictMode>
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-
-    <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ErrorBoundary>
- 
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
